97627a030b2594b208f615101cb9a9d8529770de
Shivam Maharshi <shivam.maharshi@gmail.com>
2016-06-26 17:20:17 -0400

    Delete mediawiki_1.26.2.sh

--- ubuntu_15/mediawiki_1.26.2.sh
+++
@@ -1,5 +0,0 @@
-sudo wget "https://releases.wikimedia.org/mediawiki/1.26/mediawiki-1.26.2.tar.gz"
-sudo tar -xvzf mediawiki-1.26.2.tar.gz
-sudo mv mediawiki-1.26.2 wiki
-sudo chmod 777 -R wiki
-php /var/www/wiki/maintenance/install.php --dbname testwiki --dbserver 127.0.0.1 --dbtype mysql --dbuser root --email shivam.maharshi@gmail.com --pass "shivam123" --installdbuser root --scriptpath /wiki --admin shivam
