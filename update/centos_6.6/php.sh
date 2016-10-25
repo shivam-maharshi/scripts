549122ea5ac055b745e2d7643b6ea95e421057be
Shivam Maharshi <shivam.maharshi@gmail.com>
2016-10-24 22:03:12 -0400

    Upgrade to PHP 7.0 on CentOS 6.6

---
+++ update/centos_6.6/php.sh
@@ -0,0 +1,7 @@
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+yum install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
+yum install yum-utils
+yum-config-manager --enable remi-php70
+yum update
+yum install php-7
+php --version
