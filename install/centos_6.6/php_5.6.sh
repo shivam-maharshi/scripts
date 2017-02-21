0fe3e280d3199f12dcf247e057c932061964e558
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-02-21 18:19:08 -0500

    Update php_5.6.sh

--- install/centos_6.6/php_5.6.sh
+++ install/centos_6.6/php_5.6.sh
@@ -4,4 +4,4 @@ wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 wget https://centos6.iuscommunity.org/ius-release.rpm
 rpm -Uvh ius-release*.rpm
 yum -y update
-yum -y install php56u php56u-opcache php56u-xml php56u-mcrypt php56u-gd php56u-devel php56u-mysql php56u-intl php56u-mbstring php56u-bcmath
+yum -y install php56u php56u-opcache php56u-xml php56u-mcrypt php56u-gd php56u-devel php56u-mysql php56u-intl php56u-mbstring php56u-bcmath php-pecl-apcu
