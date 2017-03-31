b50a0c8649cbf786d847bc6957dcb73678b67915
Shivam Maharshi <shivam.maharshi@gmail.com>
2017-03-31 15:16:31 -0400

    Update php_5.6.sh

--- install/centos_6.6/php_5.6.sh
+++ install/centos_6.6/php_5.6.sh
@@ -1,3 +1,4 @@
+yum remove php php-cli php-common php-gd php-ldap php-mysql php-odbc php-pdo php-pear php-pecl-apc php-pecl-memcache php-pgsql php-soap php-xml php-xmlrpc
 yum -y update
 yum -y install epel-release
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
